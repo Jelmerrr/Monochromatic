f553dbcd6197d6494c840bd744a8602254c9fb8a
Jelmerrr <Jelljel@hotmail.com>
2023-10-16 23:30:08 +0200

    Shooting through non-euclidean stuff
    This is still done janky need to fix some stuff also it still has a feedback loop that needs to be fixed.

--- Monochromatic/Assets/Scripts/NonEuclidean/PortalCameraController.cs
+++ Monochromatic/Assets/Scripts/NonEuclidean/PortalCameraController.cs
@@ -17,7 +17,7 @@ public class PortalCameraController : MonoBehaviour
             transform.position = portal.position + playerOffsetFromPrtal; //Negative movement
         else
             transform.position = new Vector3(portal.position.x, -portal.position.y, portal.position.z) - new Vector3(playerOffsetFromPrtal.x, -playerOffsetFromPrtal.y, playerOffsetFromPrtal.z);
-        
+
         float angularDiff = Quaternion.Angle(portal.rotation, otherPortal.rotation); //Weird rotation thingy
         Quaternion portalRotDiff = Quaternion.AngleAxis(angularDiff, Vector3.up);
         Vector3 newCamDir = portalRotDiff * player_cam.forward;
